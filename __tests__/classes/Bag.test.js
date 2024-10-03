2c04da5adf9085ed9d76331843a313cd820230d2
BenDev181 <benjiro181@gmail.com>
2024-10-03 15:47:30 +0100

    commit

--- __tests__/classes/Bag.test.js
+++ __tests__/classes/Bag.test.js
@@ -1,26 +1,28 @@
-const Bag = require("../classes/Bag.js")
-const Person = require("../classes/Person.js")
+const Bag = require(`../classes/Bag.js`)
+const Person = require(`../classes/Person.js`)
+const { describe, test, expect, beforeEach } = require("@jest/globals");
 
-let bag
+describe(`Test for Bad class:`, () => {
+    
+    let bag;
 
-beforeEach(() => {
-    bag = new Bag(50, 111)
-})
-
-test("can create an instance of the Bag class", () => {
-    expect(bag instanceof Bag).toBe(true)
-})
-
-test("weight and id have been assigned correctly", () => {
-    expect(bag.weight).toBe(50)
-    expect(bag.id).toBe(111)
-})
-
-test("can get the initial owner (null) using getOwner", () => {
-    expect(bag.getOwner()).toBe(null)
-})
-
-test("update owner with a Person assigned to a Bag using assignOwner()", () => {
-    bag.assignOwner("Ben")
-    expect(bag.getOwner()).toBe("Ben")
+    beforeEach(() => {
+        bag = new Bag(10, 1234)
+    })
+    
+    test(`Creates an instance of the Bag class.`, () => {
+        expect(bag instanceof Bag).toBeTruthy();
+    })
+    test(`Assigns weight and id correctly`, () => {
+        expect(bag.weight).toBe(10);
+        expect(bag.id).toBe(1234);
+    })
+    test(`Owner is initialized with a value of null`, () => {
+        expect(bag.getOwner()).toBe(null);
+    })
+    test(`Assign a Person as the owner of the bag`, () => {
+        const person = new Person();
+        bag.assignOwner(person);
+        expect(bag.getOwner()).toEqual(person);
+    })
 })
