2ec82c535b0dcd8841a47faa5941e2cf344a86b4
BenDev181 <benjiro181@gmail.com>
2024-10-03 14:17:35 +0100

    bag and bag tests

---
+++ __tests__/classes/Person.test.js
@@ -0,0 +1,27 @@
+const Person = require("../classes/Person.js")
+
+beforeEach(() => {
+    let person = new Person("Ben", "Paris")
+})
+
+test("can create an instance of the Person class", () => {
+    expect(person instanceof Person).toBe(true)
+})
+
+test("name and destination have been assigned correctly", () => {
+    expect(person.name).toBe("Ben")
+    expect(person.destination).toBe("Paris")
+})
+
+test("can create an instance of the Person class", () => {
+    expect(person instanceof Person).toBe(true)
+})
+
+test("bags initializes as an empty array", () => {
+    expect(person.getBags()).toEqual([])
+})
+
+test("addBag() adds a bag to the bags array", () => {
+    person.addBag("bag1")
+    expect(person.getBags()).toBe(["bag1"])
+})
